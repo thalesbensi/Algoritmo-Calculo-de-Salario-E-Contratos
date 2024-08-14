702422a82a0eb8f7d9a255948851b796a62cf524
Thales Bensi <thalesbensi@outlook.com>
2024-08-14 14:21:46 -0300

    Entities add

---
+++ src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
